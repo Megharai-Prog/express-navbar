c1de4dbe2bdfeae74b4711edcd2bd3d4ebcbe9fb
Megharai-Prog <kimyufu04@gmail.com>
2025-05-18 22:55:01 -0700

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,1 +1,33 @@
+// server.js
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Serve static files from public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Route for Climate Crisis
+app.get("/climate-crisis", (req, res) => {
+  res.redirect("https://github.com/megharai-prog/climate-crisis");
+});
+
+
+// Route for Typesetting
+app.get("/typesetting", (req, res) => {
+  res.redirect("https://github.com/megharai-prog/st-huxley-college");
+});
+
+// Route for Four Algorithms
+app.get("/four-algorithms", (req, res) => {
+  res.redirect("https://github.com/megharai-prog/four-algorithms");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
